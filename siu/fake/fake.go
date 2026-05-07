4463acc8100d84e595e1c6746f70d687469b91b2
usuariogenerico123 <negritolinuxero@gmail.com>
2026-05-07 22:45:30 +0000

    add banner

--- siu/fake/fake.go
+++ siu/fake/fake.go
@@ -14,7 +14,7 @@ import (
 
 func main(){
 	
-	fmt.Println(style.Banner)
+	fmt.Println(style.Banner2)
 	
 	SaveinfFile := false
 	option := os.Args
--- siu/fake/style/banner.go
+++ siu/fake/style/banner.go
@@ -5,8 +5,22 @@ import "fmt"
 
 
 
+const AUTOR string = "@mikis2323"
 
-var Banner string = fmt.Sprintf(`%s
+
+var Banner2 string = fmt.Sprintf(`%s
+
+ ____                       ____ ____  _   _ 
+|  _ \ ___  ___ ___  _ __  / ___|  _ \| \ | |
+| |_) / _ \/ __/ _ \| '_ \| |   | | | |  \| |
+|  _ <  __/ (_| (_) | | | | |___| |_| | |\  |
+|_| \_\___|\___\___/|_| |_|\____|____/|_| \_| 
+	subDomain & CDN scanner%s
+	%sby:telegram > %s  %s
+`, GREEN, END, YELLOW, AUTOR, END )
+
+
+var _ string = fmt.Sprintf(`%s
 888b     d888 d8b 888      d8b           .d8888b.  8888888b.  888b    888 
 8888b   d8888 Y8P 888      Y8P          d88P  Y88b 888  "Y88b 8888b   888 
 88888b.d88888     888                   888    888 888    888 88888b  888 
@@ -15,7 +29,7 @@ var Banner string = fmt.Sprintf(`%s
 888  Y8P  888 888 888888K  888 "Y8888b. 888    888 888    888 888  Y88888 
 888   "   888 888 888 "88b 888      X88 Y88b  d88P 888  .d88P 888   Y8888 
 888       888 888 888  888 888  88888P'  "Y8888P"  8888888P"  888    Y888 
-by:%s telegram %s%s@pes528 %s`,GREEN,END,WHITE, SUB, END  )
+by:%s telegram %s%s %s %s`,GREEN,END,WHITE, AUTOR, SUB, END  )
 
 
 
